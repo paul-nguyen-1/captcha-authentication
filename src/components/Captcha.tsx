82a4ef117010718f252a413304cc42736f0bad55
paul-nguyen96 <paul121596@icloud.com>
2023-02-25 16:52:20 -0600

    set up captcha component and styling

---
+++ src/components/Captcha.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Captcha = () => {
+  //Map out arr of images to store in grid
+  const captchaImages: string[] = new Array(9)
+    .fill(null)
+    .map((value, index : number) => {
+      return `/api/ImageAPI?index=${index}.webp`;
+    });
+
+  return (
+    <div className="captchaContainer">
+      <h2>Select All The Jim&#39;s:</h2>
+      <div className="captchaImages">
+        {captchaImages.map((image, index) => (
+          <React.Fragment key={index}>
+            <div>
+              <img src={image} alt= {image} />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Captcha
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Captcha from "@/components/Captcha";
+
 export default function Home() {
   return (
     <div className="App">
-
+      <div>
+        <input type="text" placeholder="Send a Message"></input>
+        <Captcha />
+        <button>Send</button>
+      </div>
     </div>
   )
 }
