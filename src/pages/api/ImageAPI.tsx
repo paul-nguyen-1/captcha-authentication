c9fc4b932036eadbd5bff8ce547b6446b3a65a08
paul-nguyen96 <paul121596@icloud.com>
2023-02-28 01:41:36 -0600

    pls work vercel

--- src/pages/api/ImageAPI.tsx
+++ src/pages/api/ImageAPI.tsx
@@ -1,13 +1,14 @@
 import fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withIronSessionApiRoute } from 'iron-session/next';
+import path from 'path';
 
 interface MySession {
   captchaImages?: string[];
 }
 
+//Set up path for API to randomize images for it to either be Jim or another character
 export function newCaptchaImages() {
-  //Set up path for API to randomize images for it to either be Jim or another character
   const correctImage = 0.5;
   return (new Array(9))
     .fill(null)
@@ -15,13 +16,13 @@ export function newCaptchaImages() {
       const jimCaptcha = Math.random() < correctImage;
       const imgIndex = Math.floor(Math.random() * (jimCaptcha ? 10 : 13) + 1);
       const character = (jimCaptcha ? 'jim' : 'notjim') + imgIndex + '.webp';
-      return `/images/${character}`;
+      return path.join(process.cwd(), 'public', 'images', character);
     });
 }
 
+// Export cookies to keep same captcha after each reload
+// Return API images into captcha array
 const ironSessionHandler = withIronSessionApiRoute(
-  // Export cookies to keep same captcha after each reload
-  // Return API images into captcha array
   async function handler(req: NextApiRequest, res: NextApiResponse) {
     const index = parseInt(req.query.index as string);
     const session = req.session as MySession;
@@ -30,7 +31,7 @@ const ironSessionHandler = withIronSessionApiRoute(
       await req.session.save();
     }
     res.setHeader('Content-Type', 'image/webp');
-    const imageBuffer = fs.readFileSync(`public${session.captchaImages[index]}`);
+    const imageBuffer = fs.readFileSync(session.captchaImages[index]);
     res.send(imageBuffer);
   },
   {
